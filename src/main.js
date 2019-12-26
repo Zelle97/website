185e7f5d9e2b09b0c23381ab8612a21a960bccdf
Fabian Zeller <zeller.fabian.97@web.de>
2019-12-26 15:01:56 +0100

    Implemented scroll-to for navbar items

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import VueI18n from 'vue-i18n'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import App from './App.vue'
+import VueScrollTo from 'vue-scrollto'
+
+Vue.use(VueScrollTo, {
+  container: "body",
+  duration: 700,
+  easing: "ease",
+  offset: -200,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true
+});
 
 Vue.use(Buefy);
 Vue.use(VueI18n);
--- src/sections/Navbar.vue
+++ src/sections/Navbar.vue
@@ -2,7 +2,8 @@
   <fixed-header>
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="#">
+        <span class="top" id="top"></span>
+        <a class="navbar-item" href="#" v-scroll-to="'#top'">
           <img src="../assets/Logo.png">
         </a>
         <span class="navbar-burger burger" data-target="navMenu">
@@ -14,19 +15,19 @@
 
       <div id="navMenu" class="navbar-menu">
         <div class="navbar-end">
-          <a class="navbar-item" href="#cv">
+          <a class="navbar-item" href="#" v-scroll-to="'#cv'">
             {{ $t("navbar.cv") }}
           </a>
 
-          <a class="navbar-item" href="#skills">
+          <a class="navbar-item" href="#" v-scroll-to="'#skills'">
             {{ $t("skills.header") }}
           </a>
 
-          <a class="navbar-item" href="#references">
+          <a class="navbar-item" href="#" v-scroll-to="'#references'">
             {{ $t("references.header") }}
           </a>
 
-          <a class="navbar-item" href="#contact">
+          <a class="navbar-item" href="#" v-scroll-to="'#contact'">
             {{ $t("contact.header") }}
           </a>
 
