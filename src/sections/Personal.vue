308f3f790fe41b87f32cc636c546c76c0bf7c9be
Fabian Zeller <zeller.fabian.97@web.de>
2020-09-30 18:42:19 +0200

    Added Personal Component

---
+++ src/sections/Personal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <div class="level">
+        <div class="level-item">
+          <figure class="image">
+            <img class="is-rounded" src="https://via.placeholder.com/600.png ">
+          </figure>
+        </div>
+        <div class="container has-text-centered personal-text">
+          <h1 class="title">
+            {{ $t("personal.header") }}
+          </h1>
+          <h2 class="subtitle">
+            <div class="content">
+              {{ $t("personal.text") }}
+            </div>
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "Personal"
+  }
+</script>
+
+<style scoped>
+
+  .personal-text {
+    max-width: 40vw;
+  }
+</style>
