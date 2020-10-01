c8bc3f54926832e971712f99f05d6138011bdf3b
Fabian Zeller <zeller.fabian.97@web.de>
2020-10-01 22:41:06 +0200

    Uncommited changes from update

--- src/components/LanguageDropdown.vue
+++ src/components/LanguageDropdown.vue
@@ -2,44 +2,38 @@
   <div>
     <b-dropdown position="is-bottom-left" aria-role="list">
       <button class="button" slot="trigger">
-        <b-icon
-          icon="earth"
-          size="is-small"
-        ></b-icon>
-        <b-icon
-          icon="menu-down"
-        ></b-icon>
+        <b-icon icon="earth" size="is-small"></b-icon>
+        <b-icon icon="menu-down"></b-icon>
       </button>
 
       <b-dropdown-item aria-role="listitem" @click="setLocale('en')">
-        <img src="../assets/gb.svg" class="flag-icon"> {{ $t("navbar.languages.english") }}
+        <img src="../assets/gb.svg" class="flag-icon" />
+        {{ $t("navbar.languages.english") }}
       </b-dropdown-item>
       <b-dropdown-item aria-role="listitem" @click="setLocale('de')">
-        <img src="../assets/de.svg" class="flag-icon"> {{ $t("navbar.languages.german") }}
+        <img src="../assets/de.svg" class="flag-icon" />
+        {{ $t("navbar.languages.german") }}
       </b-dropdown-item>
     </b-dropdown>
   </div>
 </template>
 
 <script>
-  import Icon from "buefy";
-
-  export default {
-    name: "LanguageDropdown",
-    components: {Icon},
-    methods: {
-      setLocale(locale){
-        this.$i18n.locale = locale;
-      }
+export default {
+  name: "LanguageDropdown",
+  components: {},
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale;
     }
   }
+};
 </script>
 
 <style scoped>
-  .flag-icon {
-    max-width: 16px;
-    max-height: 16px;
-    margin-right: 2rem;
-  }
-
+.flag-icon {
+  max-width: 16px;
+  max-height: 16px;
+  margin-right: 2rem;
+}
 </style>
--- src/sections/Contact.vue
+++ src/sections/Contact.vue
@@ -3,123 +3,130 @@
     <div class="container">
       <div class="has-text-centered">
         <span class="anchor" id="contact"></span>
-        <h1 class="title">{{$t("contact.header")}}</h1>
+        <h1 class="title">{{ $t("contact.header") }}</h1>
         <h2 class="subtitle">
-          {{$t("contact.text")}}
+          {{ $t("contact.text") }}
         </h2>
-
       </div>
 
-      <form id="contact-form" @submit="">
-
+      <form id="contact-form">
         <b-field :label="$t('contact.name')">
-          <b-input id="form-name" v-model="senderName" type="name" required="required"
-                   :placeholder="$t('contact.ph_name')"></b-input>
+          <b-input
+            id="form-name"
+            v-model="senderName"
+            type="name"
+            required="required"
+            :placeholder="$t('contact.ph_name')"
+          ></b-input>
         </b-field>
 
         <b-field :label="$t('contact.email')">
-          <b-input id="form-mail"
-                   v-model="fromMail"
-                   type="email"
-                   icon="email"
-                   :placeholder="$t('contact.ph_email')">
+          <b-input
+            id="form-mail"
+            v-model="fromMail"
+            type="email"
+            icon="email"
+            :placeholder="$t('contact.ph_email')"
+          >
           </b-input>
         </b-field>
 
         <b-field :label="$t('contact.message')">
-          <b-input id="form-message" v-model="content" maxlength="600" type="textarea" required="required"
-                   :placeholder="$t('contact.ph_message')"></b-input>
+          <b-input
+            id="form-message"
+            v-model="content"
+            maxlength="600"
+            type="textarea"
+            required="required"
+            :placeholder="$t('contact.ph_message')"
+          ></b-input>
         </b-field>
 
         <div class="has-text-centered">
           <vue-recaptcha
             ref="recaptcha"
             sitekey="6Ledt8UUAAAAAEh1smA69WF4f8TsSXve6Wl5BO_J"
-            @verify="onCaptchaVerified">
-            <b-button type="is-info"
-                      outlined
-                      rounded>
+            @verify="onCaptchaVerified"
+          >
+            <b-button type="is-info" outlined rounded>
               {{ $t("contact.submit_button") }}
             </b-button>
           </vue-recaptcha>
-
         </div>
-
       </form>
-
     </div>
   </section>
 </template>
 
 <script>
+import axios from "axios";
+import VueRecaptcha from "vue-recaptcha";
 
-  import axios from "axios";
-  import VueRecaptcha from "vue-recaptcha"
-
-  export default {
-    name: "Contact",
-    components: {VueRecaptcha},
-    data() {
-      return {
-        loading: false,
-        "senderName": "",
-        "fromMail": "",
-        "content": ""
+export default {
+  name: "Contact",
+  components: { VueRecaptcha },
+  data() {
+    return {
+      loading: false,
+      senderName: "",
+      fromMail: "",
+      content: ""
+    };
+  },
+  methods: {
+    onCaptchaVerified: function(token) {
+      if (this.verifyInput()) {
+        this.verifyToken(token);
+      } else {
+        this.showInputErrors();
+        this.showError(this.$t("contact.form_input_error"));
+        this.$refs.recaptcha.reset();
       }
     },
-    methods: {
-      onCaptchaVerified: function (token) {
-        if (this.verifyInput()) {
-          this.verifyToken(token);
-        } else {
-          this.showInputErrors();
-          this.showError(this.$t('contact.form_input_error'));
-          this.$refs.recaptcha.reset();
-        }
-      },
-      verifyInput: function () {
-        return document.getElementById("form-name").checkValidity() &&
-          document.getElementById("form-mail").checkValidity() &&
-          document.getElementById("form-message").checkValidity();
-      },
-      showInputErrors: function () {
-        document.getElementById("form-name").reportValidity();
-        document.getElementById("form-mail").reportValidity();
-        document.getElementById("form-message").reportValidity();
-      },
-      verifyToken: function (token) {
-        let that = this;
-        axios.post('https://backend.zellerfabian.de/submitForm', {
+    verifyInput: function() {
+      return (
+        document.getElementById("form-name").checkValidity() &&
+        document.getElementById("form-mail").checkValidity() &&
+        document.getElementById("form-message").checkValidity()
+      );
+    },
+    showInputErrors: function() {
+      document.getElementById("form-name").reportValidity();
+      document.getElementById("form-mail").reportValidity();
+      document.getElementById("form-message").reportValidity();
+    },
+    verifyToken: function(token) {
+      let that = this;
+      axios
+        .post("https://backend.zellerfabian.de/submitForm", {
           senderName: this.senderName,
           fromMail: this.fromMail,
           content: this.content,
           "g-recaptcha-response": token
         })
-          .then(function (response) {
-            that.showSuccess(that.$t('contact.form_submit_success'))
-          })
-          .catch(function (error) {
-            that.showError(that.$t('contact.form_submit_error'))
-          })
-      },
-      showError: function (message) {
-        this.$buefy.toast.open({
-          message: message,
-          position: 'is-bottom-left',
-          type: 'is-danger'
+        .then(function() {
+          that.showSuccess(that.$t("contact.form_submit_success"));
         })
-      },
-      showSuccess: function (message) {
-        this.$buefy.toast.open({
-          message: message,
-          position: 'is-bottom-right',
-          type: 'is-success'
-        })
-      }
+        .catch(function() {
+          that.showError(that.$t("contact.form_submit_error"));
+        });
+    },
+    showError: function(message) {
+      this.$buefy.toast.open({
+        message: message,
+        position: "is-bottom-left",
+        type: "is-danger"
+      });
+    },
+    showSuccess: function(message) {
+      this.$buefy.toast.open({
+        message: message,
+        position: "is-bottom-right",
+        type: "is-success"
+      });
     }
   }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
